9e05daa99856038dcb7f6e2138fa66b3f534da8a
Roman Holovin <danteek@gmail.com>
2017-04-25 12:02:26 +0300

    fix: test dependency on the current date

--- tests/__fixtures__/consumption.js
+++ tests/__fixtures__/consumption.js
@@ -17,13 +17,16 @@ export const initial = {
   }),
 };
 
-const today = format(Date.now(), DATE_KEY_FORMAT);
+export const defaultDateInPast = new Date(2017, 3, 22);
+export const defaultDateNow = new Date(2017, 3, 24);
+
+const dateKey = format(defaultDateNow, DATE_KEY_FORMAT);
 export const oneDay = {
   [STORAGE_KEY]: JSON.stringify({
     version: 1,
     drinks: ['water', 'tea'],
     consumption: {
-      [today]: [{
+      [dateKey]: [{
         type: 'water',
         amount: 0.3,
       }, {
