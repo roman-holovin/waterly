27ea27b52bf8f34b77a62219ca1f0db2920f8d26
Roman Holovin <danteek@gmail.com>
2017-04-26 16:42:39 +0300

    feat: initialize storage with initial set of data

--- tests/__fixtures__/consumption.js
+++ tests/__fixtures__/consumption.js
@@ -1,5 +1,5 @@
 import format from 'date-fns/format';
-import { STORAGE_KEY, DATE_KEY_FORMAT } from '../../src/constants';
+import { STORAGE_KEY, DATE_KEY_FORMAT, INITIAL_STATE } from '../../src/constants';
 
 export const empty = {
   [STORAGE_KEY]: null,
@@ -10,11 +10,7 @@ export const malformed = {
 };
 
 export const initial = {
-  [STORAGE_KEY]: JSON.stringify({
-    version: 1,
-    drinks: ['water', 'tea'],
-    consumption: {},
-  }),
+  [STORAGE_KEY]: JSON.stringify(INITIAL_STATE),
 };
 
 export const defaultDateInPast = new Date(2017, 3, 22);
@@ -22,17 +18,21 @@ export const defaultDateNow = new Date(2017, 3, 24);
 
 const dateKey = format(defaultDateNow, DATE_KEY_FORMAT);
 export const oneDay = {
-  [STORAGE_KEY]: JSON.stringify({
-    version: 1,
-    drinks: ['water', 'tea'],
-    consumption: {
-      [dateKey]: [{
-        type: 'water',
-        amount: 0.3,
-      }, {
-        type: 'tea',
-        amount: 0.5,
-      }],
-    },
-  }),
+  [STORAGE_KEY]: JSON.stringify(
+    Object.assign(
+      {},
+      INITIAL_STATE,
+      {
+        consumption: {
+          [dateKey]: [{
+            type: 'water',
+            amount: 0.3,
+          }, {
+            type: 'tea',
+            amount: 0.5,
+          }],
+        },
+      }
+    )
+  ),
 }
