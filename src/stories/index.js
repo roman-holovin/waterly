35beeda53efb1714edeba8c31457ed5ec0107fbe
Roman Holovin <danteek@gmail.com>
2017-04-27 17:12:09 +0300

    chore: add storybook integration

---
+++ src/stories/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+
+storiesOf('Header', module)
+  .add('default', () => (
+    <header>header</header>
+  ));
